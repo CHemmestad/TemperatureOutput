a45f5df3f9bcb0d2c7609ac76cb0b0ba8b0d1fd3
Caleb <73370112+CHemmestad@users.noreply.github.com>
2021-10-04 10:02:32 -0500

    Update Temperature Output.cxx
    Adding functions

--- Temperature Output.cxx
+++ Temperature Output.cxx
@@ -17,46 +17,105 @@ temperatures.
 
 using namespace std ; //So I dont have to do std::
 
+//Precondition ; Checking to see if the file we want exists or can be found
+//Postcondition ; Creating the file we need if it cant be found
+int createFile ( ) ;
+
+//Precondition ; We dont have the number we need to make the chart the correct scale when printed
+//Postcondition ; Doing math to get the numbver we need for making the chart
+int findTempChartMin ( int tempMin ) ;
+
+//Precondition ; We dont have the number we need to make the chart the correct scale when printed
+//Postcondition ; Doing math to get the numbver we need for making the chart 
+int findTempChartMax ( int tempMax ) ;
+
+//Precondition ; We dont have any information to display on the chart yet
+//Postcondition ; Getting information from the file to use on the chart
+void getInfoFromFile ( double &tempNumber , bool done , int tempMin , int tempMax , int tempChartMin ) ;
+
+//Precondition ; There isnt a bottom to the chart
+//Postcondition ; Making the bottom of the chart with the temp min and max displayed for clarity
+void makeChartBottom ( int tempMax , int tempMin , int tempChartMax , int tempChartMin ) ;
+
 int main(int argc, char **argv)
 {
-	
-	char yesNo ; //Variable used for asking user to create file if file not found
-	
-	//Opening the file and creating a new one if needed
+	//Checking if the file exists first and creating it if it doesnt
 	ifstream infile ;
 	infile.open("TempInput.dat") ; //Opening the file we want the information from
 	if ( !infile ) { //Checking if the file exists or can be found
-		cout << "File not found" << endl ;
-		cout << "Do you want to create the file? Y/N : " ;
-		cin >> yesNo ; //Asking the user if they want the program to create the file for them
-		if ( yesNo == 'N' || yesNo == 'n' ) {
-			return 1 ;
-		} else if ( yesNo == 'Y' || yesNo == 'y' ) {
-			ofstream outfile ; 
-			outfile.open("TempInput.dat");
-			outfile << "(Enter hourly temperatures in this file)" ;
-			outfile.close( );
-		} else {
-			cout << "Invalid input. Program ended." ;
-			return 2 ;
-		}
+		createFile ( ) ; //Creating the file if it doesnt
 	}
+	infile.close ( ) ; //Closing the file
 	
-	
+	//Variables
 	int tempMin = -35 ; //Variable to change the range of the minimum temp for the chart
 	int tempMax = 120 ; //Variable to chang the range of the maximum temp for the chart
 	double tempNumber ; //Used to store the values from the file and use whithin the loop
-	bool done = false ; //Used to tell the loop when to stop
+	bool done = false ; //Used to tell the loop when to stop (kind of useless)
+	int tempChartMin = findTempChartMin ( tempMin ) ; //Variable used for changing the actual size of the chart with the values given
+	int tempChartMax = findTempChartMax ( tempMax ) ; //Variable used for changing the size of the chart
 	
 	system ("clear") ; //Making it look cleaner
 	
 	//Printing the heading for the scale of the chart
 	cout << "\t\t\t    " << tempMin << " - " << tempMax << " degrees hourly" << endl << endl ;
 	
-	int tempChartMin = (tempMin*-1)/3+1 ; //Variable used for changing the actual size of the chart with the values given
-	int tempChartMax = tempMax/3+1 ; //Variable used for changing the size of the chart
+	//Taking numbers from inside the file and then printing the chart
+	getInfoFromFile ( tempNumber , done , tempMin , tempMax , tempChartMin ) ;
 	
+	//Creating the bottom line for the bottom of the chart with the range displayed for clarity
+	makeChartBottom ( tempMax , tempMin , tempChartMax , tempChartMin ) ;
+		
+	return 0;
+}
+
+void makeChartBottom ( int tempMax , int tempMin , int tempChartMax , int tempChartMin ) {
+	cout << "\t   " ;
+	cout << tempMin ;
+	for ( int x = 0 ; x < tempChartMax+tempChartMin ; x++ ) {
+		cout << '_' ;
+	}
+	cout << tempMax ;
+}
+
+int createFile ( ) {
+	char yesNo ; //Variable used for asking user to create file if file not found
+	
+	//Asking if they want the program to create the file for them
+	cout << "File not found" << endl ;
+	cout << "Do you want to create the file? Y/N : " ;
+	cin >> yesNo ; //Asking the user if they want the program to create the file for them
+	if ( yesNo == 'N' || yesNo == 'n' ) {
+		return 1 ;
+	} else if ( yesNo == 'Y' || yesNo == 'y' ) {
+		ofstream outfile ; 
+		outfile.open("TempInput.dat");
+		outfile << "(Enter hourly temperatures in this file)" ;
+		outfile.close( );
+	} else {
+		cout << "Invalid input. Program ended." ;
+		return 2 ;
+	}
+	return 0 ;
+}
+
+int findTempChartMin ( int tempMin ) { 
+	int chartMin ;
+	chartMin = (tempMin*-1)/3+1 ; 
+	return chartMin ;
+}
+
+int findTempChartMax ( int tempMax ) { 
+	int chartMax ; 
+	chartMax = tempMax/3+1 ;
+	return chartMax ;
+}
+
+void getInfoFromFile ( double &tempNumber , bool done , int tempMin , int tempMax , int tempChartMin ) {
 	//Creating a loop to take all the information from the file
+	ifstream infile ;
+	infile.open("TempInput.dat") ; //Opening the file we want the information from
+	
 	while ( done == false ) {
 		if ( infile != '\0' ) {
 			infile >> tempNumber ;
@@ -102,17 +161,6 @@ int main(int argc, char **argv)
 			done = true ;
 		}
 	}
-	
-	//Creating the bottom line for the bottom of the chart with the range displayed for clarity
-	cout << "\t   " ;
-	cout << tempMin ;
-	for ( int x = 0 ; x < tempChartMax+tempChartMin ; x++ ) {
-		cout << '_' ;
-	}
-	cout << tempMax ;
-	
-	infile.close () ; //Closing the file
-	
-	return 0;
+	infile.close ( ) ;
 }
 
